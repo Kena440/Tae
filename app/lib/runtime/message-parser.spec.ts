ae5f67dadf0be2011d6d173f46f32a35d04c1bd8
Kena440 <amukenam1@gmail.com>
2025-09-01 11:02:42 +0200

    chore: rename Bolt to Tae

--- app/lib/runtime/message-parser.spec.ts
+++ app/lib/runtime/message-parser.spec.ts
@@ -28,23 +28,23 @@ describe('StreamingMessageParser', () => {
       ['Foo bar <', 'Foo bar '],
       ['Foo bar <p', 'Foo bar <p'],
       [['Foo bar <', 's', 'p', 'an>some text</span>'], 'Foo bar <span>some text</span>'],
-    ])('should correctly parse chunks and strip out bolt artifacts (%#)', (input, expected) => {
+    ])('should correctly parse chunks and strip out tae artifacts (%#)', (input, expected) => {
       runTest(input, expected);
     });
   });
 
   describe('invalid or incomplete artifacts', () => {
     it.each<[string | string[], ExpectedResult | string]>([
-      ['Foo bar <b', 'Foo bar '],
+      ['Foo bar <t', 'Foo bar '],
       ['Foo bar <ba', 'Foo bar <ba'],
-      ['Foo bar <bol', 'Foo bar '],
-      ['Foo bar <bolt', 'Foo bar '],
-      ['Foo bar <bolta', 'Foo bar <bolta'],
-      ['Foo bar <boltA', 'Foo bar '],
-      ['Foo bar <boltArtifacs></boltArtifact>', 'Foo bar <boltArtifacs></boltArtifact>'],
-      ['Before <oltArtfiact>foo</boltArtifact> After', 'Before <oltArtfiact>foo</boltArtifact> After'],
-      ['Before <boltArtifactt>foo</boltArtifact> After', 'Before <boltArtifactt>foo</boltArtifact> After'],
-    ])('should correctly parse chunks and strip out bolt artifacts (%#)', (input, expected) => {
+      ['Foo bar <ta', 'Foo bar '],
+      ['Foo bar <tae', 'Foo bar '],
+      ['Foo bar <taea', 'Foo bar <taea'],
+      ['Foo bar <taeA', 'Foo bar '],
+      ['Foo bar <taeArtifacs></taeArtifact>', 'Foo bar <taeArtifacs></taeArtifact>'],
+      ['Before <oltArtfiact>foo</taeArtifact> After', 'Before <oltArtfiact>foo</taeArtifact> After'],
+      ['Before <taeArtifactt>foo</taeArtifact> After', 'Before <taeArtifactt>foo</taeArtifact> After'],
+    ])('should correctly parse chunks and strip out tae artifacts (%#)', (input, expected) => {
       runTest(input, expected);
     });
   });
@@ -52,14 +52,14 @@ describe('StreamingMessageParser', () => {
   describe('valid artifacts without actions', () => {
     it.each<[string | string[], ExpectedResult | string]>([
       [
-        'Some text before <boltArtifact title="Some title" id="artifact_1">foo bar</boltArtifact> Some more text',
+        'Some text before <taeArtifact title="Some title" id="artifact_1">foo bar</taeArtifact> Some more text',
         {
           output: 'Some text before  Some more text',
           callbacks: { onArtifactOpen: 1, onArtifactClose: 1, onActionOpen: 0, onActionClose: 0 },
         },
       ],
       [
-        ['Some text before <boltArti', 'fact', ' title="Some title" id="artifact_1">foo</boltArtifact> Some more text'],
+        ['Some text before <taeArti', 'fact', ' title="Some title" id="artifact_1">foo</taeArtifact> Some more text'],
         {
           output: 'Some text before  Some more text',
           callbacks: { onArtifactOpen: 1, onArtifactClose: 1, onActionOpen: 0, onActionClose: 0 },
@@ -67,12 +67,12 @@ describe('StreamingMessageParser', () => {
       ],
       [
         [
-          'Some text before <boltArti',
+          'Some text before <taeArti',
           'fac',
           't title="Some title" id="artifact_1"',
           ' ',
           '>',
-          'foo</boltArtifact> Some more text',
+          'foo</taeArtifact> Some more text',
         ],
         {
           output: 'Some text before  Some more text',
@@ -81,11 +81,11 @@ describe('StreamingMessageParser', () => {
       ],
       [
         [
-          'Some text before <boltArti',
+          'Some text before <taeArti',
           'fact',
           ' title="Some title" id="artifact_1"',
           ' >fo',
-          'o</boltArtifact> Some more text',
+          'o</taeArtifact> Some more text',
         ],
         {
           output: 'Some text before  Some more text',
@@ -94,13 +94,13 @@ describe('StreamingMessageParser', () => {
       ],
       [
         [
-          'Some text before <boltArti',
+          'Some text before <taeArti',
           'fact tit',
           'le="Some ',
           'title" id="artifact_1">fo',
           'o',
           '<',
-          '/boltArtifact> Some more text',
+          '/taeArtifact> Some more text',
         ],
         {
           output: 'Some text before  Some more text',
@@ -109,11 +109,11 @@ describe('StreamingMessageParser', () => {
       ],
       [
         [
-          'Some text before <boltArti',
+          'Some text before <taeArti',
           'fact title="Some title" id="artif',
           'act_1">fo',
           'o<',
-          '/boltArtifact> Some more text',
+          '/taeArtifact> Some more text',
         ],
         {
           output: 'Some text before  Some more text',
@@ -121,13 +121,13 @@ describe('StreamingMessageParser', () => {
         },
       ],
       [
-        'Before <boltArtifact title="Some title" id="artifact_1">foo</boltArtifact> After',
+        'Before <taeArtifact title="Some title" id="artifact_1">foo</taeArtifact> After',
         {
           output: 'Before  After',
           callbacks: { onArtifactOpen: 1, onArtifactClose: 1, onActionOpen: 0, onActionClose: 0 },
         },
       ],
-    ])('should correctly parse chunks and strip out bolt artifacts (%#)', (input, expected) => {
+    ])('should correctly parse chunks and strip out tae artifacts (%#)', (input, expected) => {
       runTest(input, expected);
     });
   });
@@ -135,20 +135,20 @@ describe('StreamingMessageParser', () => {
   describe('valid artifacts with actions', () => {
     it.each<[string | string[], ExpectedResult | string]>([
       [
-        'Before <boltArtifact title="Some title" id="artifact_1"><boltAction type="shell">npm install</boltAction></boltArtifact> After',
+        'Before <taeArtifact title="Some title" id="artifact_1"><taeAction type="shell">npm install</taeAction></taeArtifact> After',
         {
           output: 'Before  After',
           callbacks: { onArtifactOpen: 1, onArtifactClose: 1, onActionOpen: 1, onActionClose: 1 },
         },
       ],
       [
-        'Before <boltArtifact title="Some title" id="artifact_1"><boltAction type="shell">npm install</boltAction><boltAction type="file" filePath="index.js">some content</boltAction></boltArtifact> After',
+        'Before <taeArtifact title="Some title" id="artifact_1"><taeAction type="shell">npm install</taeAction><taeAction type="file" filePath="index.js">some content</taeAction></taeArtifact> After',
         {
           output: 'Before  After',
           callbacks: { onArtifactOpen: 1, onArtifactClose: 1, onActionOpen: 2, onActionClose: 2 },
         },
       ],
-    ])('should correctly parse chunks and strip out bolt artifacts (%#)', (input, expected) => {
+    ])('should correctly parse chunks and strip out tae artifacts (%#)', (input, expected) => {
       runTest(input, expected);
     });
   });
--- app/types/actions.ts
+++ app/types/actions.ts
@@ -13,6 +13,6 @@ export interface ShellAction extends BaseAction {
   type: 'shell';
 }
 
-export type BoltAction = FileAction | ShellAction;
+export type TaeAction = FileAction | ShellAction;
 
-export type BoltActionData = BoltAction | BaseAction;
+export type TaeActionData = TaeAction | BaseAction;
